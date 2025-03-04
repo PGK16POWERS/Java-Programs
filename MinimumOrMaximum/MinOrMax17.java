b2cf5dd1c0734d1e28fe315f211721cf9080680b
programmer Kartier <progrmmerKartier@gmail.com>
2025-03-04 18:18:32 +0200

    java

--- MinimumOrMaximum/MinOrMax17.java
+++ MinimumOrMaximum/MinOrMax17.java
@@ -4,5 +4,12 @@ import java.util.Arrays;
 public class MinOrMax17 {
     public static void main(String[] args) {
         // CREATE ARRAY
+        int[] numberOfCars = {2, 3, 4,6,2};
+
+        int maxInt = Arrays.stream(numberOfCars).max().orElseThrow();
+        int minInt = Arrays.stream(numberOfCars).min().orElseThrow();
+
+        System.out.println("The highest number in the array is: " + maxInt);
+        System.out.println("The lowest number in the array is: " + minInt);
     }
 }
